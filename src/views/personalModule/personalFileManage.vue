f6269cfc2b65cb246e14a7dfa5926a8ed554a5a5
stoneElm <ststone_elm@163.com>
2024-07-18 00:54:18 +0800

    添加文件管理阵列显示

--- src/views/personalModule/personalFileManage.vue
+++ src/views/personalModule/personalFileManage.vue
@@ -1,86 +1,145 @@
 <template>
-    <el-button type="primary" @click="filePreview('video')">文件预览VIDEO</el-button>
-    <el-button type="primary" @click="filePreview('pdf')">文件预览PDF多</el-button>
-    <el-button type="primary" @click="filePreview('audio')">文件预览MP3</el-button>
-    <el-button type="primary" @click="filePreview('html')">文件预览HTML</el-button>
-    <el-button type="primary" @click="filePreview('text')">文件预览Text</el-button>
+    <div ref="calaWidth" style="width: 100%;"></div>
+    <div class="attach-dtl-list" v-for="attachDtlList in attachDtlListList">
+        <div class="attach-dtl-view" v-for="item in attachDtlList" :key="item.attachDtlID">
+            <div v-if="item.isShow==='02'" class="attach-dtl-view-show" v-on:dblclick="filePreviewDblclick(item.attachDtlID)">
+                <div class="attach-dtl-logo">
+                
+                </div>
+                <div class="attach-dtl-name">{{ item.attachDtlName }}</div>
+            </div>
+            <div class="attach-dtl-view-none">
+
+            </div>
+        </div>
+        
+    </div>
+    
 </template>
 
-<script>
-import { ref, reactive } from 'vue'
-import { useRoute } from 'vue-router'
-export default {
-    data() {
-        const route = useRoute();
-        
-    },
-    methods: {
-        filePreview(attachType) {
-            console.log('文件预览');
+<script setup>
+    import { ref, reactive, onMounted, onUnmounted } from 'vue';
+    import { useRouter } from 'vue-router'
 
-            let stoneFileToken = sessionStorage.getItem('stoneFileToken');
+    import {
+        queryAttachDtlList
+    } from '@/api/attachApi/attachApi'
 
-            if (!stoneFileToken) {
-                ElMessage({
-                    message: '登录过期，请重新登录！',
-                    type: 'error',
-                    duration: 3.5 * 1000
-                })
+    const route = useRouter();
+
+    const attachDtlList = ref([]);
+    const attachDtlListList = ref([]);
+    const calaWidth = ref(null);
+    const defaultWidth = ref(150);
+    const storageWidth = ref(0);
+
+    onMounted(() => {
+        window.addEventListener('resize', handleResize);
+        initAttachDtlList();
+    });
+
+    onUnmounted(() => {
+        window.removeEventListener('resize', handleResize);
+    });
+
+    function initAttachDtlList() {
+        let attachDtlQueryParam = {}
+
+        queryAttachDtlList(attachDtlQueryParam).then(res => {
+            if (res.code == '00000') {
+                console.log('--- 查询附件详情列表出参 ---', res)
+
+                res.data.forEach((value, index, array) => {
+                    value.isShow = '02';
+                });
+
+                attachDtlList.value = res.data
+
+                storageWidth.value = calaWidth.value.offsetWidth;
+                console.log('----- storageWidth -----', storageWidth.value);
+                initDtlListList();
             }
+        })
+    }
 
-            let attachDtlID;
+    function handleResize() {
+        let divWidth = calaWidth.value.offsetWidth;
+        if (divWidth - storageWidth.value >= defaultWidth.value 
+                || divWidth - storageWidth.value <= defaultWidth.value * -1) {
+            storageWidth.value = divWidth;
+            console.log('----- storageWidth -----', storageWidth.value);
+            initDtlListList();
+        }
+    }
 
-            if (attachType === 'video') {
-                attachDtlID = 1424070900000002;
+    function initDtlListList() {
+
+        let attachDtlListTemp = JSON.parse(JSON.stringify(attachDtlList.value));
+        console.log('aaa', attachDtlListTemp.length)
+
+        // 计算个数
+        let rows = Math.ceil(storageWidth.value / defaultWidth.value);
+
+        let dtlListLen = attachDtlList.value.length;
+        let line = Math.ceil(dtlListLen / rows);
+        console.log('----- 计算结果 -----', line + '行;' + rows +'列');
+
+        let addVal = line * rows - dtlListLen;
+        if (addVal > 0) {
+            for (var i = 0; i < addVal; i++) {
+                attachDtlListTemp.push([])
             }
-
-            if (attachType === 'pdf') {
-                attachDtlID = 1424070900000001;
-            }
-
-            if (attachType === 'pdf2') {
-                attachDtlID = 1424071200000001;
-            }
-
-            if (attachType === 'html') {
-                attachDtlID = 1424062100000001;
-            }
-
-            if (attachType === 'audio') {
-                attachDtlID = 1424071100000001;
-            }
-
-            if (attachType === 'text') {
-                attachDtlID = 1424071600000004;
-            }
-
-            sessionStorage.setItem('filePreviewAttachDtlID', attachDtlID);
-
-            // let routeData = this.$router.resolve({ path: '/filePreview', query: filePreview });
-            let routeData = this.$router.resolve({ path: '/filePreview'});
-
-            window.open(routeData.href, '_blank');
-            
-        },
-        
-    },
-    setup() {
-        const route = useRoute();
-    
-        function openNewTab() {
-            // 打开新标签页的逻辑
-            const newTabRoute = {
-                path: '/filePreview',
-                name: 'filePreview'
-            };
-            route.push(newTabRoute);
         }
 
-        // 其他设置...
+        let list = [];
+
+        for (var i = 0; i < attachDtlListTemp.length; i += rows) {
+            var subArray = attachDtlListTemp.slice(i, i + rows);
+            list.push(subArray);
+        }
+
+        attachDtlListList.value = list;
+        console.log('----- 二维列表 -----', attachDtlListList.value);
+
     }
-}
+
+    function filePreviewDblclick(attachDtlID) {
+        console.log('filePreviewDblclick:', attachDtlID);
+
+        sessionStorage.setItem('filePreviewAttachDtlID', attachDtlID);
+
+        // let filePreview = {};
+        // let routeData = route.resolve({ path: '/filePreview', query: filePreview });
+
+        let routeData = route.resolve({ path: '/filePreview'});
+
+        window.open(routeData.href, '_blank');
+    }
 </script>
 
 <style>
-
+.attach-dtl-list {
+    display: flex;
+}
+.attach-dtl-view {
+    flex: 1;
+    background-color: pink;
+    margin-right: 20px;
+    margin-bottom: 20px;
+}
+.attach-dtl-view-show {
+    cursor: pointer;
+    padding: 8px;
+}
+.attach-dtl-view-none {
+    cursor: pointer;
+    height: 0px;
+}
+.attach-dtl-view:nth-child(odd):last-child,
+.attach-dtl-view:nth-child(even):last-child {
+ margin-right: 0; 
+}
+.attach-dtl-name {
+    word-break: break-all;
+}
 </style>
